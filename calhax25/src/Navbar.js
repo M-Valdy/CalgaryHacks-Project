ae37feee56efe57abdee0d39d2d153da22b4c45c
M-Valdy <mevald620@gmail.com>
2025-02-16 07:43:02 -0700

    Finished front end

--- calhax25/src/Navbar.js
+++ calhax25/src/Navbar.js
@@ -1,5 +1,5 @@
 export default function Navbar() {
-    return (   <nav className="nav">
+    return (  <nav className="nav">
         <a  href="/" className="site-title">
         Site Name
     </a>
